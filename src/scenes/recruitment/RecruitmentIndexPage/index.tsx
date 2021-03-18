fcd0b26f79cf8b3e929f6fde721dede080c95b4b
westlag114 <fsww2353@gmail.com>
2021-03-18 16:40:05 +0900

    fix

--- src/scenes/recruitment/RecruitmentIndexPage/index.tsx
+++ src/scenes/recruitment/RecruitmentIndexPage/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import styles from "./style.module.scss";
 import { Link } from "react-router-dom";
-import { useCurrentAccount } from "../../../hooks/useCurrentAccount";
 
 const RecruitmentIndexPage = () => {
-  const { account } = useCurrentAccount();
   return (
     <div className={styles.text}>
-      求人一覧ページ
-      <Link to="/sign_in">サインイン</Link>
+      全ての募集
+      <Link to="/sign_in">ログイン</Link>
     </div>
   );
 };
